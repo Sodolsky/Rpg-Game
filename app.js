96ab05163833442ea36e015323e160a218d2f448
Sodolsky <mateuszsodolskiofficial@gmail.com>
2021-05-22 14:31:03 +0200

    V0.2 Basics of weapon system

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
 let firstplayer=false;
-const fightgrid=document.querySelector('.fight-grid');
+const fightgrid=document.querySelectorAll('.fight-grid');
 const obrazki=document.querySelectorAll('.obrazki');
 let FirstHP=document.querySelector('.hp1');
 let FirstATK=document.querySelector('.atak1');
 let SecondHP=document.querySelector('.hp2');
 let SecondATK=document.querySelector('.atak2');
+let NrPerFloor=0;
+let wearing=document.querySelectorAll('.sloty');
 const walkaButton=document.querySelector('#walka');
-function Red(atak,hp){
+let loot=['img/swords1.png','img/swords2.png','img/swords3.png'];
+let inventoryslots=document.querySelectorAll('.inventory-slots');
+let equipment=[false,false,false,false,false];
+let equipmentweapons=[];
+let bronie=[];
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function setURL(styleof,geticon){
+return styleof.style.backgroundImage="url("+geticon+")"
+inventoryslots[i].style.backgroundImage="url("+bronie[bronie.length-1].icon+")";;
+}
+function wylosujloot(){
+let index=Math.floor(Math.random()*3);
+return loot[index];
+}
+function Player(atak,hp){
 this.atak=atak;
 this.hp=hp;
-this.pokaz=function(){
-console.log(this.atak,this.hp);    
+}
+function Monster(atak,hp){
+this.atak=atak;
+this.hp=hp;
+this.generujnowego=function(){
+    const randatak=Math.floor(Math.random()*5)+1;    
+    const randhp=Math.floor(Math.random()*30)+10;
+this.atak=randatak;
+this.hp=randhp;    
+}    
+}
+function Miecz(){
+this.losowa=generateRandomNumber(1,100);
+let index=Math.floor(Math.random()*3);
+this.identyfikator=NrPerFloor;
+this.icon=loot[index];
+if(this.losowa<65){
+this.atak=generateRandomNumber(1,5);
+this.rarity="Common";    
+}
+else if(this.losowa>=65&&this.losowa<=95){
+this.atak=generateRandomNumber(6,9)
+this.rarity="Rare";
+}
+else if(this.losowa>95&&this.losowa<100){
+this.atak=generateRandomNumber(9,12)
+this.rarity="Epic";
+}
+else if(this.losowa===100){
+this.atak=generateRandomNumber(12,16);
+this.rarity="Legendary";
 }
 }
 function walka(u1,u2){ // Fight
 obrazki[0].style.display='none';
-fightgrid.style.opacity="1";
+fightgrid[0].style.opacity="1";
+fightgrid[1].style.opacity="1";
 if(firstplayer===false){   
 u2.hp=u2.hp-u1.atak; // /Hits 2nd target
 red(SecondHP);
@@ -27,7 +75,21 @@ u1.hp=u1.hp-u2.atak;
 red(FirstHP); // Hits 1st target
 updatefightstats(u1,u2)
 firstplayer=false;
-}  
+}
+if(currentMonster.hp<=0){
+NrPerFloor++;
+bronie.push(new Miecz());
+    for(let i=0;i<equipment.length;i++){
+        if(equipment[i]===false){
+            setURL(inventoryslots[i],bronie[bronie.length-1].icon)
+            inventoryslots[i].przypisane=bronie[bronie.length-1];
+            equipment[i]=true;
+            break;    
+        }
+}
+//inventoryslots[0].style.backgroundImage="url("+sword.icon+")";
+u2.generujnowego();   
+}
 }
 function updatefightstats(u1,u2){ //Updates stats in the fight
 FirstHP.innerHTML=`HP:${u1.hp}`;
@@ -35,33 +97,31 @@ SecondHP.innerHTML=`HP:${u2.hp}`;
 FirstATK.innerHTML=`Atak:${u1.atak}`;
 SecondATK.innerHTML=`Atak:${u2.atak}`;
 save();
-if(u1.hp===0||u2.hp===0){
-alert('koniecwalki')    
-}
 }
 function save(){ // Saves the game
 let save ={
-hp1:wojo.hp,
-hp2:wojo2.hp,    
+hp1:gracz.hp,
+hp2:currentMonster.hp,    
 }
 localStorage.setItem('save',JSON.stringify(save));    
 }
 function load(){ // Loades the game
 let savedstate=JSON.parse(localStorage.getItem('save'));
-fightgrid.style.opacity="0";
+fightgrid[0].style.opacity="0";
+fightgrid[1].style.opacity="0";
 if (savedstate!=null&&savedstate!=undefined){
-wojo.hp=savedstate.hp1;
-wojo2.hp=savedstate.hp2;
-updatefightstats(wojo,wojo2)    
+gracz.hp=savedstate.hp1;
+currentMonster.hp=savedstate.hp2;
+updatefightstats(gracz,currentMonster)    
 }
 else{
-updatefightstats(wojo,wojo2)    
+updatefightstats(gracz,currentMonster)    
 }   
 } 
-let wojo=new Red(10,40);
-let wojo2=new Red(10,50);
+let gracz=new Player(5,100);
+let currentMonster=new Monster(3,25);
 walkaButton.addEventListener('click',()=>{
-walka(wojo,wojo2) 
+walka(gracz,currentMonster) 
 })
 //Reset
 document.querySelector('#reset').addEventListener('click',()=>{localStorage.clear()
@@ -69,8 +129,58 @@ window.location.reload();
 });
 function red(myElement){
 let aktualnykolor=getComputedStyle(myElement).color;
-console.log(aktualnykolor);
 myElement.style.color="red";
 setTimeout(()=>{myElement.style.color=aktualnykolor;},1000)
-}    
+}  
 load();
+for (let i=0;i<inventoryslots.length;i++){
+inventoryslots[i].addEventListener('click',()=>{
+if(inventoryslots[i].przypisane!=undefined){
+    if(inventoryslots[i].przypisane!=undefined&&wearing[0].przypisane!=undefined){
+    console.log(inventoryslots[i].przypisane.atak,wearing[0].przypisane.atak)
+    let placeholder=wearing[0].przypisane;
+    gracz.atak-=wearing[0].przypisane.atak;
+    wearing[0].przypisane=inventoryslots[i].przypisane;
+    setURL(wearing[0],inventoryslots[i].przypisane.icon)
+    gracz.atak+=wearing[0].przypisane.atak;
+    inventoryslots[i].przypisane=placeholder;
+    setURL(inventoryslots[i],placeholder.icon)
+    updatefightstats(gracz,currentMonster);
+    }
+else{
+    wearing[0].przypisane=inventoryslots[i].przypisane;
+    setURL(wearing[0],inventoryslots[i].przypisane.icon)
+    event.target.przypisane=undefined;
+    event.target.style.backgroundImage="none";
+    equipment[i]=false;
+    equipitem();
+    updatefightstats(gracz,currentMonster);
+}
+}
+})
+}
+function equipitem(){
+for(let i=0;i<wearing.length;i++){
+if(wearing[i].przypisane!=undefined){
+gracz.atak+=wearing[i].przypisane.atak;   
+}    
+}
+}
+function unequiptitem(){
+    for(let i=0;i<wearing.length;i++){
+    wearing[i].addEventListener('click',()=>{
+        if(wearing[i].przypisane!=undefined){
+            if(equipment[i]===false){
+                setURL(inventoryslots[i],wearing[i].przypisane.icon)
+                inventoryslots[i].przypisane=wearing[i].przypisane;
+                equipment[i]=true;    
+            }
+            gracz.atak-=wearing[i].przypisane.atak;
+            updatefightstats(gracz,currentMonster)   
+            event.target.przypisane=undefined;
+        event.target.style.backgroundImage="none";
+        }    
+    })
+}
+}
+unequiptitem();    
