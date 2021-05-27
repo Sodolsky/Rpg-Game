68f0b6599d7cd6593572da87dda5925a1a8bd526
Sodolsky <mateuszsodolskiofficial@gmail.com>
2021-05-27 10:32:59 +0200

    V0.4 XP BAR

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,22 @@
 //const { default: tippy } = require("tippy.js")
 //const Flatted = require("flatted");
 //const Flatted = require("flatted");
+//const Flatted = require("flatted");
 let firstplayer=false;
+let stage=[1,1];
+let nroffight=1;
+let level=1;
+let stagedisplay=document.querySelector('.stage');
+let leveldispay=document.querySelector('.level');
+let xpvalue=document.querySelector('.xpvalue')
+var elem = document.getElementById("myBar");
 const fightgrid=document.querySelectorAll('.fight-grid');
 const obrazki=document.querySelectorAll('.obrazki');
 let FirstHP=document.querySelector('.hp1');
 let FirstATK=document.querySelector('.atak1');
 let SecondHP=document.querySelector('.hp2');
 let SecondATK=document.querySelector('.atak2');
+let kulki=document.querySelectorAll('.kolka');
 let tytuły=[['Tępy','Moczymordy','Obszczymura','Denata','Parobka','Zjeba'],
 ['Ostry','Rzezimieszka','Twojego taty','Syna koleżanki twojej starej','Rycerza','Kamila Z'],
 ['Epicki','Syna Kowala','Księcia','Odkrywcy','Korisa','Twojego Wujka'],
@@ -46,6 +55,65 @@ this.atak=randatak;
 this.hp=randhp;    
 }    
 }
+function ProgressBar() {
+    let i = 0;
+    this.xp=1000;
+    this.basexp=0;
+    this.przelicznik=Math.floor(this.xp/100);
+    this.gainedxp=false;
+    this.move=function(xp,przelicznik,basexp){
+        if (i == 0) {
+        i = 1;
+        var width;
+        width=1;
+        var id = setInterval(frame, 500);
+        function frame() {
+            if(bar.gainedxp){
+            let rand=generateRandomNumber(6,8);
+            basexp+=Math.floor(xp/rand);
+            bar.gainedxp=false;    
+            }
+
+            percentage=Math.floor(basexp/przelicznik);
+            width=percentage;
+            if (width >= 100) {
+            clearInterval(id);
+            elem.style.width='1%';
+            elem.innerHTML='1%';
+            i=0;
+            bar.rerender();
+            xpvalue.innerHTML=basexp+'/'+xp;
+            } else{
+            xpvalue.innerHTML=Math.floor(basexp)+'/'+Math.floor(xp);
+            elem.style.width = width + "%";
+            elem.innerHTML = Math.floor(width) + "%";
+            }
+        }
+        }
+    }
+    this.rerender=function(){
+    clearInterval(timer);
+    Math.floor(this.xp=this.xp*1.06);
+    this.basexp=1;
+    this.przelicznik=Math.floor(this.xp/100);
+    level++;
+    updatelevel();
+    this.move(this.xp,this.przelicznik,this.basexp); 
+    }
+    this.loadlevel=function(){
+    for(let i=0;i<level;i++){
+        Math.floor(this.xp=this.xp*1.06);
+        this.basexp=1;
+        this.przelicznik=Math.floor(this.xp/100); 
+    }
+    this.move(this.xp,this.przelicznik,this.basexp); 
+    }
+    let timer=setInterval(() => {
+    this.move(this.xp,this.przelicznik,this.basexp);
+    }, 1000);    
+}
+let bar=new ProgressBar();
+/////////Bronie
 function Miecz(){
 let rand=generateRandomNumber(1,5);
 this.losowa=generateRandomNumber(1,100);
@@ -56,9 +124,6 @@ if(this.losowa<65){
 this.atak=generateRandomNumber(1,5);
 this.rarity="Common"; 
 this.kolor="gray";
-this.penis=function(){
-console.log(this.atak);    
-}
 this.title=`${tytuły[0][0]} miecz ${tytuły[0][rand]}`  
 }
 else if(this.losowa>=65&&this.losowa<=95){
@@ -93,6 +158,12 @@ this.opis=function(parent){
         this.nazwa.destroy();
 }   
 }
+function updatestage(){
+stagedisplay.innerHTML=`Stage: ${stage[0]}-${stage[1]}`
+}
+function updatelevel(){
+leveldispay.innerHTML=`Level: ${level}`
+}
 function walka(u1,u2){ // Fight
 obrazki[0].style.display='none';
 fightgrid[0].style.opacity="1";
@@ -110,6 +181,26 @@ updatefightstats(u1,u2)
 firstplayer=false;
 }
 if(currentMonster.hp<=0){
+bar.gainedxp=true;
+for (const i of kulki) {
+if(i.classList.contains('wypelnione')){
+if(nroffight===5){
+stage[1]++;
+for (let i of kulki) {
+i.classList.remove('wypelnione');    
+}
+updatestage();
+nroffight=1;
+break;
+}
+continue;   
+}
+else{
+nroffight++;
+i.classList.add('wypelnione');
+break;
+}
+} 
 NrPerFloor++;
 bronie.push(new Miecz());
     for(let i=0;i<equipment.length;i++){
@@ -117,7 +208,6 @@ bronie.push(new Miecz());
             setURL(inventoryslots[i],bronie[bronie.length-1].icon)
             inventoryslots[i].przypisane=bronie[bronie.length-1];
             inventoryslots[i].przypisane.opis(inventoryslots[i]);
-            console.log(inventoryslots[i].przypisane)
             equipment[i]=true;
             //test();
             break;    
@@ -135,22 +225,33 @@ SecondATK.innerHTML=`Atak:${u2.atak}`;
 save();
 }
 let ftime=false;
+let ftimeeq=false;
 function save(){ // Saves the game
 let save ={
 hp1:gracz.hp,
 hp2:currentMonster.hp,
+level:level,
+stage:stage,
+basexp:bar.basexp,
 }
 let eq=zapiszeq();
+let equbrane=zapiszubrane();
 if(inventoryslots[0].przypisane!=undefined){
     ftime=true;
     localStorage.setItem('ftime',ftime);    
 }
+if(wearing[0].przypisane||wearing[1].przypisane||wearing[2].przypisane){
+ftimeeq=true;
+localStorage.setItem('ftimeeq',ftimeeq);    
+}
 localStorage.setItem('eqsave',Flatted.stringify(eq));
-localStorage.setItem('save',JSON.stringify(save));    
+localStorage.setItem('save',JSON.stringify(save));  
+localStorage.setItem('equbrane',Flatted.stringify(equbrane))  
 }
 function load(){ // Loades the game
 let savedstate=JSON.parse(localStorage.getItem('save'));
 ftime=localStorage.getItem('ftime');
+ftimeeq=localStorage.getItem('ftimeeq');
 if(ftime){
 var savedeq=Flatted.parse(localStorage.getItem('eqsave'));
 }
@@ -171,15 +272,45 @@ if(savedeq!=undefined){
                 this.nazwa.destroy();
         }   
         equipment[i]=true;
-        console.log(equipment);
             inventoryslots[i].przypisane.opis(inventoryslots[i]);
     }
+}
+if(ftimeeq){
+var savedubrane=Flatted.parse(localStorage.getItem('equbrane'));
+if(savedubrane!=undefined){
+    for (let i=0;i<savedubrane.length;i++){
+        wearing[i].przypisane=savedubrane[i];
+        setURL(wearing[i],wearing[i].przypisane.icon);
+        wearing[i].przypisane.opis=function(parent){
+            this.nazwa=tippy(parent,{
+                theme:'informacja',
+                allowHTML:true,
+                content:`
+                ${this.title}<br>
+                Atak: ${this.atak}<br>
+                Rarity: <span style="color:${this.kolor};">${this.rarity}</span>
+                `,})}
+                wearing[i].przypisane.usunopis=function(){
+                    this.nazwa.destroy();
+            }   
+                equipitem();
+                updatefightstats(gracz,currentMonster)
+                wearing[i].przypisane.opis(wearing[i]);
+        }    
+}
 }
 fightgrid[0].style.opacity="0";
 fightgrid[1].style.opacity="0";
 if (savedstate!=null&&savedstate!=undefined){
 gracz.hp=savedstate.hp1;
 currentMonster.hp=savedstate.hp2;
+level=savedstate.level;
+stage=savedstate.stage;
+bar.loadlevel();
+console.log(savedstate.basexp);
+bar.basexp=savedstate.basexp;
+updatelevel();
+updatestage();
 updatefightstats(gracz,currentMonster)    
 }
 else{
@@ -280,4 +411,13 @@ function zapiszeq(){
     }
 }
     return arrayforsave;
+};
+function zapiszubrane(){
+    let arrayforsaveubrane=[];
+    for (let i = 0; i <wearing.length; i++) {
+    if(wearing[i].przypisane!=undefined){
+    arrayforsaveubrane.push(wearing[i].przypisane)
+    }
+}
+    return arrayforsaveubrane;
 };
